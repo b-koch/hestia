54fe7db1f590456c6370784f9de6b2ec74043ea3
B-Koch <bernd.koch@mailbox.org>
2026-07-29 16:57:28 +0200

    create /opt/Filen before installing Filen.IO, v7

--- build_files/scripts/external/filen-io.sh
+++ build_files/scripts/external/filen-io.sh
@@ -2,20 +2,9 @@
 
 set -euox pipefail
 
-echo "Debug Info..."
-ls -la / | grep -E 'opt|var|usr'
-stat /opt 2>&1 || echo "opt does not exist"
-
 echo "Installing Filen..."
 
-curl -Lo /tmp/filen.rpm https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
-mkdir -p /tmp/filen-extract
-rpm2cpio /tmp/filen.rpm | cpio -idmv -D /tmp/filen-extract
-
-mkdir -p /opt/Filen
-cp -a /tmp/filen-extract/opt/Filen/. /opt/Filen/
-
-cp -a /tmp/filen-extract/usr/share/applications/*.desktop /usr/share/applications/ 2>/dev/null || true
-rm -rf /tmp/filen.rpm /tmp/filen-extract
+dnf5 install -y \
+    https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
 
 echo "Filen installed successfully."
