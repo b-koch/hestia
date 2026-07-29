cdd380c87b70a73201266371628d2f113d3c3a74
B-Koch <bernd.koch@mailbox.org>
2026-07-29 16:02:39 +0200

    create /opt/Filen before installing Filen.IO, v2

--- build_files/scripts/external/filen-io.sh
+++ build_files/scripts/external/filen-io.sh
@@ -4,7 +4,7 @@ set -euox pipefail
 
 echo "Installing Filen..."
 
-mkdir -p /opt/Filen
+install -d /opt/Filen
 
 dnf5 install -y \
     https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
