f122c38e12903aa5f354c15bd4348bbbd207fc35
B-Koch <bernd.koch@mailbox.org>
2026-08-02 12:07:36 +0200

    sysext stuff

--- sysext/build_sysext.sh
+++ sysext/build_sysext.sh
@@ -155,15 +155,12 @@ find "$ROOTFS" \
     -exec rm -rf {} +
 
 
-rm -f \
-    "$ROOTFS/usr/lib/os-release" \
-    "$ROOTFS/etc/os-release"
+rm -f "$ROOTFS/usr/lib/os-release"
 
 rm -rf "$ROOTFS/usr/share/man"
 rm -rf "$ROOTFS/usr/share/doc"
 rm -rf "$ROOTFS/usr/share/info"
 rm -rf "$ROOTFS/usr/share/licenses"
-rm -rf "$ROOTFS/var"
 
 
 echo "--- Building SquashFS ---"
