46bbd26a556c2019e0361003f59751b0eefa4466
B-Koch <bernd.koch@mailbox.org>
2026-08-02 03:14:40 +0200

    sudoers for hestia sysext update

--- build_files/scripts/append-topgrade.sh
+++ build_files/scripts/append-topgrade.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo '"Hestia sysexts" = "/usr/libexec/hestia-sysext-fetch.sh"' \
+echo '"Hestia sysexts" = "sudo /usr/libexec/hestia-sysext-fetch.sh"' \
     >> /etc/ublue-os/topgrade.toml
+
+
