3159c88d019cc3dd3b7e90350c3d96340b061f6e
B-Koch <bernd.koch@mailbox.org>
2026-08-23 12:06:14 +0200

    Fix malformed ujust file.

--- build_files/scripts/modify_kargs.sh
+++ build_files/scripts/modify_kargs.sh
@@ -1,3 +1,3 @@
-# Append kernel quirk to fix the ASUS ROG Azoth keyboard sometimes not working.
+# Append kernel quirk to fix t^
 # HID_QUIRK_ALWAYS_POLL (0x00000400) and HID_QUIRK_NOGET (0x00000008)
 bootc kargs --append=usbhid.quirks=0x0b05:0x1ace:0x00000408
