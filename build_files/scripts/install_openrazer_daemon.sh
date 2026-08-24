e91f381c31e395786b218c7f6e3f425e8418e70e
B-Koch <bernd.koch@mailbox.org>
2026-08-24 11:25:56 +0200

    Disable scriptlets for openrazer-daemon installation.

--- build_files/scripts/install_openrazer_daemon.sh
+++ build_files/scripts/install_openrazer_daemon.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-dnf5 install -y --setopt=install_weak_deps=False \
-    --exclude=openrazer-kernel-modules-dkms --exclude=dkms \
-    openrazer-daemon
+dnf5 install -y --setopt=tsflags=noscripts openrazer-daemon
+
+if ! grep -q "^plugdev:" /etc/group; then
+    grep "^plugdev:" /lib/group >> /etc/group
+fi
