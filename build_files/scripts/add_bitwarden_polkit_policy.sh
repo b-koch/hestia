0ab2d750d1b6a00ab1d7aea4bd9eac219b94792a
B-Koch <bernd.koch@mailbox.org>
2026-08-27 16:49:39 +0200

    Remove chown part of the bitwarden policy script

--- build_files/scripts/add_bitwarden_polkit_policy.sh
+++ build_files/scripts/add_bitwarden_polkit_policy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
-echo "Downloadaing policy to /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy..."
-wget -O /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy https://raw.githubusercontent.com/bitwarden/clients/main/apps/desktop/resources/com.bitwarden.desktop.policy
+POLICY="/usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy"
 
-echo "Changing owner for /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy..."
-chown root:root /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+echo "Downloading policy to $POLICY..."
+wget --no-hsts -O "$POLICY" https://raw.githubusercontent.com/bitwarden/clients/main/apps/desktop/resources/com.bitwarden.desktop.policy
 
-echo "Changing security context for /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy..."
-chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
+echo "Changing owner for $POLICY..."
+chown root:root "$POLICY"
+chmod 0644 "$POLICY"
