c308aa192bef875d6103e46db7827e566af899f7
B-Koch <bernd.koch@mailbox.org>
2026-09-19 09:17:37 +0200

    Correct wording.

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Copy the contents of system_files/ of the git repo to /
 cp -avf "/ctx/system_files"/. /
 
-echo "Ensuring shell scripts are executable..."
+echo "Ensuring hestia shell scripts are executable..."
 find /usr/lib/hestia -type f -name '*.sh' -exec chmod +x {} +
 
 echo "Adding repositories..."
